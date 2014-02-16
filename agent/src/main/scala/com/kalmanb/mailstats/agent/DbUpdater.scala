ea667c1141a1d0839a6cbbdae46e12a4f388e80b
kalmanb <kalman@movio.co>
2014-02-16 13:20:38 +1300

    Add simple cassandra insert for SentFrom

--- agent/src/main/scala/com/kalmanb/mailstats/agent/DbUpdater.scala
+++ agent/src/main/scala/com/kalmanb/mailstats/agent/DbUpdater.scala
@@ -1,11 +1,71 @@
 package com.kalmanb.mailstats.agent
 
 import akka.actor._
+import com.datastax.driver.core._
+import scala.collection.JavaConversions._
 
 class DbUpdater extends Actor {
-   
+  lazy val cluster = Cluster.builder.addContactPoint("localhost").build
+
   def receive = {
-    case s: SummaryData =>
+    case s: SummaryData ⇒
+  }
+
+  def updateSent(fromDomain: String, server: String, queue: String, sent: Integer) = {
+    val session = cluster.connect
+
+    val statement = new BoundStatement(session.prepare("""
+      INSERT INTO mailstats.SentFrom 
+      (fromDomain, server, queue, time, sent) 
+      values (?,?,?,dateOf(NOW()),?)
+      """))
+    statement.bind(fromDomain, server, queue, sent)
+    try {
+      session.execute(statement)
+    } catch { case _: Exception ⇒ println("oh no") }
+
+    session.shutdown
   }
   
+  def testGet(fromDomain:String, server:String, queue:String):List[Row] = {
+    val session = cluster.connect
+
+    val statement = new BoundStatement(session.prepare("""
+      select * from mailstats.SentFrom 
+      where  
+        fromDomain = ?
+        and server = ?
+        and queue = ? 
+      """))
+    statement.bind(fromDomain, server, queue )
+    val results = session.execute(statement).all
+
+    session.shutdown
+    results.toList
+  }
+
+  override def postStop() = cluster.shutdown
 }
+
+//case class SummaryData(
+//val server: String,
+//val queue: String,
+//val sentFromDomain: mutable.Map[String, Int],
+//val sentToDomain: mutable.Map[String, Int])
+
+/*
+ *
+CREATE KEYSPACE mailstats WITH replication 
+ = {'class':'SimpleStrategy', 'replication_factor':1};
+
+CREATE TABLE mailstats.SentFrom (
+  fromDomain varchar, 
+  server varchar,
+  queue varchar, 
+  time timestamp,
+  sent int,
+  PRIMARY KEY ((fromDomain,server, queue),time)
+);
+
+
+*/
--- agent/src/test/scala/com/kalmanb/mailstats/agent/DbUpdaterTest.scala
+++ agent/src/test/scala/com/kalmanb/mailstats/agent/DbUpdaterTest.scala
@@ -1,12 +1,56 @@
 package com.kalmanb.mailstats.agent
 
 import com.kalmanb.test.TestSpec
+import akka.actor._
+import akka.testkit._
+import com.datastax.driver.core._
+import scala.collection.JavaConversions._
 
 class DbUpdaterTest extends TestSpec {
+  implicit val system = ActorSystem("test")
 
   describe("db updater") {
     it("should send sent data to cassandra") {
+      val updater = TestActorRef[DbUpdater](Props(new DbUpdater()))
 
+      truncate("SentFrom")
+
+      updater.underlyingActor.updateSent("domain", "server", "queue", 1)
+      updater.underlyingActor.updateSent("domain", "server", "queue", 2)
+
+      val results = testGet("domain", "server", "queue")
+      results.size should be(2)
+      results(0).getInt("sent") should be(1)
+      results(1).getInt("sent") should be(2)
+
+      updater.stop
     }
+  }
+
+  def testGet(fromDomain: String, server: String, queue: String): List[Row] = {
+    lazy val cluster = Cluster.builder.addContactPoint("localhost").build
+    val session = cluster.connect
+
+    val statement = new BoundStatement(session.prepare("""
+      select * from mailstats.SentFrom 
+      where  
+        fromDomain = ?
+        and server = ?
+        and queue = ? 
+      """))
+    statement.bind(fromDomain, server, queue)
+    val results = session.execute(statement).all
+
+    session.shutdown
+    results.toList
+  }
+
+  def truncate(table: String) = {
+    lazy val cluster = Cluster.builder.addContactPoint("localhost").build
+    val session = cluster.connect
+
+    session.execute(s"truncate mailstats.$table")
+
+    session.shutdown
   }
 }
